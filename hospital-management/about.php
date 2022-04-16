d926c9102ff86ec08230e245fc87b7ad8d9cbfaa
Gopas-coder <53084994+Gopas-coder@users.noreply.github.com>
2022-04-17 00:28:13 +0530

    files for hospital management

---
+++ hospital-management/about.php
@@ -0,0 +1,53 @@
+<?php include 'header.php';?>
+
+
+<!-- Content -->
+<div id="content">
+
+    <!-- Intro -->
+    <section class="p-t-b-150">
+        <div class="container">
+            <div class="intro-main">
+                <div class="row">
+
+                    <!-- Intro Detail -->
+                    <div class="col-md-7">
+                        <div class="text-sec padding-right-0">
+                            <h5>Health Check Ups</h5>
+                            <p>patients can check their health conditions.</p>
+                            <ul class="row">
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> EMERGENCY CASE</h6>
+                                    <p>24X7 emergency services </p>
+                                </li>
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> QUALIFIED DOCTORS</h6>
+                                    <p>Expert doctors with years of experience </p>
+                                </li>
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> ONLINE APPOINTMENT</h6>
+                                    <p>Patients can make online appoinment before visiting the hospital</p>
+                                </li>
+                                <li class="col-sm-6">
+                                    <h6> <i class="lnr  lnr-checkmark-circle"></i> FREE MEDICAL COUNSELING</h6>
+                                    <p>Our Hospital Management system provides free medical counceling as a part of mental health awareness.</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- Intro Timing -->
+                    <div class="col-md-5"> <img class="img-responsive intro-img" src="images/intro-img.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</div>
+
+<!-- Footer -->
+<!--======= FOOTER =========-->
+
+<?php include 'footer.php';?>
---
+++ hospital-management/index.php
@@ -0,0 +1,324 @@
+
+  <?php include 'header.php';?>
+  <!-- Bnr Header -->
+  <section class="main-banner">
+    <div class="tp-banner-container">
+      <div class="tp-banner">
+        <ul>
+          
+          <!-- SLIDE  -->
+          <li data-transition="random" data-slotamount="7" data-masterspeed="300"  data-saveperformance="off" > 
+            <!-- MAIN IMAGE --> 
+
+            <img src="images/hms.jpg"  alt="slider"  data-bgposition=" center center" data-bgfit="cover" data-bgrepeat="no-repeat"
+            style="margin-top: 3%"> 
+
+            
+            <!-- LAYER NR. 1 -->
+            
+              <!--       data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="4" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.4" 
+                data-endspeed="300" -->
+            <!-- <div class = "hospital_name"
+              style="position: relative; top: 50%">
+              <h2>this is your hospital <h2>
+
+            </div> -->
+            <!-- LAYER NR. 2 -->
+            <div class="tp-caption sfr tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="100" 
+                data-speed="800" 
+                data-start="1000" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                style="z-index: 6; font-size:30px; color:gray; font-weight:500; margin-top: 5%; white-space: nowrap;">We care about your health </div>
+            
+            <!-- LAYER NR. 3 -->
+            <div class="tp-caption sfb tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="300" 
+                data-speed="800" 
+                data-start="1200" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="none" 
+                data-splitout="none" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                style="z-index: 7;  font-size:22px; color:gray; font-weight:500; max-width: auto; max-height: auto; white-space: nowrap;">Best Hospitality Services in your town</div>
+            
+            <!-- LAYER NR. 4 -->
+            <div class="tp-caption lfb tp-resizeme scroll" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="200"
+                data-speed="800" 
+                data-start="1300"
+                data-easing="Power3.easeInOut" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                data-scrolloffset="0"
+                style="z-index: 8;"><a href="#." class="btn">Book Now</a> </div>
+          </li>
+          
+          <!-- SLIDE  -->
+          <li data-transition="random" data-slotamount="7" data-masterspeed="300"  data-saveperformance="off" > 
+            <!-- MAIN IMAGE --> 
+            <img src="images/hms2.jpg"  alt="slider"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat"> 
+            
+            <!-- LAYER NR. 1 -->
+            <div class="tp-caption sfl tp-resizeme" 
+                data-x="left" data-hoffset="400" 
+                data-y="center" data-voffset="-100" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.4" 
+                data-endspeed="300"
+                style="z-index: 5; font-size:40px; font-weight:500; color:#000;  max-width: auto; max-height: auto; white-space: nowrap;">Best Diagnostic Centre </div>
+            
+            <!-- LAYER NR. 2 -->
+            <div class="tp-caption sfr tp-resizeme" 
+                data-x="left" data-hoffset="400" 
+                data-y="center" data-voffset="-40" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.4" 
+                data-endspeed="300"
+                style="z-index: 5; font-size:55px; font-weight:500; color:#000;  max-width: auto; max-height: auto; white-space: nowrap;">Care And Cure</div>
+            
+            <!-- LAYER NR. 3 -->
+            <div class="tp-caption sfb tp-resizeme" 
+                data-x="left" data-hoffset="400" 
+                data-y="center" data-voffset="30" 
+                data-speed="800" 
+                data-start="1400" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="none" 
+                data-splitout="none" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                style="z-index: 7; font-size:16px; color:#000; font-weight:500; line-height:26px; max-width: auto; max-height: auto; white-space: nowrap;">Improved diagnostic performance and heightened satisfaction of patients <br> and physicians delight.</div>
+            
+            <!-- LAYER NR. 4 -->
+            <div class="tp-caption lfb tp-resizeme scroll" 
+                data-x="left" data-hoffset="400" 
+                data-y="center" data-voffset="140"
+                data-speed="800" 
+                data-start="1600"
+                data-easing="Power3.easeInOut" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                data-scrolloffset="0"
+                style="z-index: 8;"><a href="#." class="btn">CONTACT NOW</a> </div>
+          </li>
+          
+          <!-- SLIDE  -->
+          <li data-transition="random" data-slotamount="7" data-masterspeed="300"  data-saveperformance="off" > 
+            <!-- MAIN IMAGE --> 
+            <img src="images/hms3.jpg"  alt="slider"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat"> 
+            
+            <!-- LAYER NR. 2 -->
+            <div class="tp-caption sfb tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-80" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                data-scrolloffset="0"
+                style="z-index: 6; font-size:40px; color:#000; font-weight:500; white-space: nowrap;"> Welcome To Our Research Center </div>
+            
+            <!-- LAYER NR. 3 -->
+            <div class="tp-caption sfb tp-resizeme text-center" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-10" 
+                data-speed="800" 
+                data-start="1000" 
+                data-easing="Power3.easeInOut" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                data-scrolloffset="0"
+                style="z-index: 7; font-size:20px; font-weight:500; line-height:26px; color:#000; max-width: auto; max-height: auto; white-space: nowrap;">We work in a friendly and efficient using the latest <br>
+              technologies and sharing our expertise.</div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  
+  <!-- Content -->
+  <div id="content"> 
+    
+    <!-- Intro -->
+    <section class="p-t-b-150">
+      <div class="container">
+        <div class="intro-main">
+          <div class="row"> 
+            
+            <!-- Intro Detail -->
+            <div class="col-md-8">
+              <div class="text-sec">
+                <h5>Health Check Ups</h5>
+                <p>Besides providing world class clinical lab services, Labaid Diagnostic Centre houses a pool of doctors of different medical specialty to serve the ailing patients as outpatients. They are all reputed and respected in their medical specialty for outstanding clinical management</p>
+                <ul class="row">
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> EMERGENCY CASE</h6>
+                    <p>Excepteur sint occaecat cupidatat non roident, 
+                      sunt in culpa qui officia deserunt mollit </p>
+                  </li>
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> QUALIFIED DOCTORS</h6>
+                    <p>Excepteur sint occaecat cupidatat non roident, 
+                      sunt in culpa qui officia deserunt mollit </p>
+                  </li>
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> ONLINE APPOINTMENT</h6>
+                    <p>Excepteur sint occaecat cupidatat non roident, 
+                      sunt in culpa qui officia deserunt mollit </p>
+                  </li>
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> FREE MEDICAL COUNSELING</h6>
+                    <p>Excepteur sint occaecat cupidatat non roident, 
+                      sunt in culpa qui officia deserunt mollit </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            
+            <!-- Intro Timing -->
+            <div class="col-md-4">
+              <div class="timing"> <i class="lnr lnr-clock"></i>
+                <ul>
+                  <li> Monday <span>8.00 - 16.00</span></li>
+                  <li> Tuesday <span>8.00 - 16.00</span></li>
+                  <li> Wednesday <span>8.00 - 16.00</span></li>
+                  <li> Thursday <span>8.00 - 16.00</span></li>
+                  <li> Friday <span>8.00 - 16.00</span></li>
+                  <li> Saturday <span>8.00 - 16.00</span></li>
+                  <li> Sunday <span>8.00 - 16.00</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+  
+
+    
+    <!-- DOCTOR LIST -->
+    <section class="p-t-b-150">
+      <div class="container"> 
+        
+        <!-- Heading -->
+        <div class="heading-block">
+          <h2>Our Services</h2>
+          <hr>
+          <span>Expertise Services on all fields </span> </div>
+        
+        <!-- Services -->
+        <div class="services">
+          <div class="row"> 
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-eye-2 icon"></i> </div>
+                <div class="media-body">
+                  <h6>Eye Specialist</h6>
+                  <p>Expert eye specialists  who specializes in the diagnosis and treatment of diseases of the eye. Ophthalmologists may prescribe and fit eyeglasses and contact lenses and also treat eye diseases with drugs and surgery.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-operating-room icon"></i> </div>
+                <div class="media-body">
+                  <h6>Operation Theater</h6>
+                  <p>Hospital facility within a hospital where surgical operations are carried out in an aseptic environment.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-icu-monitor icon"></i> </div>
+                <div class="media-body">
+                  <h6>ICU Department</h6>
+                  <p>Special department of a hospital or health care facility that provides intensive care medicine</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-doctor icon"></i> </div>
+                <div class="media-body">
+                  <h6>Qualified Doctors</h6>
+                  <p>Doctors who holds a degree recognised by the Medical Council of India and is registered by the Medical Council of any State.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-heartbeat icon"></i> </div>
+                <div class="media-body">
+                  <h6>Heart Problems</h6>
+                  <p>Diagnosing heart related problems with all equipments facilites .</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-stomach-2 icon"></i> </div>
+                <div class="media-body">
+                  <h6>Stomach Problems</h6>
+                  <p>Diagnosing stomach related problems with all equipments and facilites.</p>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+   
+    
+    
+  </div>
+  
+  <!-- Footer -->
+<?php include 'footer.php';?>
